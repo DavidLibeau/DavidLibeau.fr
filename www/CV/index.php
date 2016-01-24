cae74471ebac3e747fd0c3690c939acc5fc255bf
DavidLibeau <davidl4@laposte.net>
2016-01-24 20:23:57 +0100

    Update CV download links

--- www/CV/index.php
+++ www/CV/index.php
@@ -7,8 +7,8 @@
 
         <div style="position: fixed; margin: 10px 10px; padding: 10px 10px; background-color: rgba(0,0,0,0.5); bottom: 0; left: 0; color: white; -webkit-border-radius: 10px; -moz-border-radius: 10px; border-radius: 10px;">
         <h4 style="margin: 0; margin-bottom: 4px;">T&eacute;l&eacute;charger mon CV</h4>
-        <a class="btn btn-small btn-primary" href="CV-DavidLibeau.png" download="CV_DavidLibeau"><i class="fa fa-download"></i> .png</a>
-        <a class="btn btn-small btn-primary" href="CV-DavidLibeau.pdf" download="CV_DavidLibeau"><i class="fa fa-download"></i> .pdf</a>
+        <a class="btn btn-small btn-primary" href="DavidLibeau_CV.jpg" download="CV_DavidLibeau"><i class="fa fa-download"></i> .jpg</a>
+        <a class="btn btn-small btn-primary" href="DavidLibeau_CV.pdf" download="CV_DavidLibeau"><i class="fa fa-download"></i> .pdf</a>
         </div>
         <div class="content" style="background: rgba(0,0,0,0); padding: 0; border: 0;">
             <img src="DavidLibeau_CV.jpg" style="width: 100%;"/>
