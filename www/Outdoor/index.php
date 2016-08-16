abf6b0dbebf52789d88e77b2f6722fd0ed0a96da
DavidLibeau <davidl4@laposte.net>
2016-08-16 13:47:30 +0200

    Adding wave (Vive les vacances !)

--- www/Outdoor/index.php
+++ www/Outdoor/index.php
@@ -1,19 +1,19 @@
-
-        <?php require "../resrc/html/AllHead.html"; ?>
-        <title>David Libeau - On parle de moi</title>
-    </head>
-    <body>
-        <?php require "../resrc/html/AllNav.html"; ?>
-        <div class="content">
-            <h1>On parle de moi</h1>
-            <p>"Ce visage me dit quelque chose !", "David Libeau ?!", vous m'avez peut-&ecirc;tre vu, lu ou entendu quelque part. Ici, toutes mes apparitions dans la presse sont rassembl&eacute;es.</p>
-            <br/>
-            <h3>Le d&eacute;fi LEGRIA mini de Canon</h3>
-            <br/>
-            <iframe width="100%" height="480" src="//www.youtube.com/embed/u6xZAg5lRkQ?rel=0" frameborder="0" allowfullscreen></iframe>
-            <br/>
-            <iframe width="100%" height="480" src="//www.youtube.com/embed/ABQ5cITaulM?rel=0" frameborder="0" allowfullscreen></iframe>
-            <br/><br/><br/><br/><br/><br/>
-        </div>
-    </body>
-</html>
+        <?php require "../resrc/html/AllHead.html"; ?>
+        <title>David Libeau - On parle de moi</title>
+    </head>
+    <body>
+        <?php require "../resrc/html/AllNav.html"; ?>
+        <div class="content">
+            <h1>On parle de moi</h1>
+            <p>"Ce visage me dit quelque chose !", "David Libeau ?!", vous m'avez peut-&ecirc;tre vu, lu ou entendu quelque part. Ici, toutes mes apparitions dans la presse sont rassembl&eacute;es.</p>
+            <br/>
+            <h3>Le d&eacute;fi LEGRIA mini de Canon</h3>
+            <br/>
+            <iframe width="100%" height="480" src="//www.youtube.com/embed/u6xZAg5lRkQ?rel=0" frameborder="0" allowfullscreen></iframe>
+            <br/>
+            <iframe width="100%" height="480" src="//www.youtube.com/embed/ABQ5cITaulM?rel=0" frameborder="0" allowfullscreen></iframe>
+            <br/><br/>            <h3>#EntrezDansLeGame</h3>
+            <br/>            <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FGoogleFrance%2Fphotos%2Fa.1746812025599013.1073741832.1594820090798208%2F1746813018932247%2F%3Ftype%3D3&width=500" width="500" height="375" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>            <br/><br/><br/><br/><br/>
+        </div>
+    </body>
+</html>
