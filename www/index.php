c6ce22eae1033fbd255dc731639139a4c4fef602
David Libeau <DavidLibeau@users.noreply.github.com>
2017-01-03 14:04:53 +0100

    Remove shit

--- www/index.php
+++ www/index.php
@@ -218,18 +218,6 @@
 </div>
 
 
-<script>
-    (function (i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-            (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date(); a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
-    ga('create', 'UA-49518822-1', 'davidlibeau.fr');
-    ga('send', 'pageview');
-    
-</script>
 <script type="text/javascript" src="//dav.li/jquery/2.1.4.js"></script>
 <script type="text/javascript" src="//dav.li/jquery/ui/jquery-ui.js"></script>
 <script type="text/javascript" src="/resrc/js/jquery.mousewheel.min.js"></script>
